641130a092330e3eed8f9bd0ffb4c793f0a8eb50
jlcanovas <me@jlcanovas.es>
2012-02-24 13:34:00 +0000

    second "first commit" :)
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/collaboro/trunk@17 ad8d3fff-70aa-a484-856a-f7f39f4f7e1d

---
+++ plugins/fr.inria.atlanmod.collaboro.history/src/fr/inria/atlanmod/collaboro/history/Proposal.java
@@ -0,0 +1,151 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package fr.inria.atlanmod.collaboro.history;
+
+import org.eclipse.emf.common.util.EList;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Proposal</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link fr.inria.atlanmod.collaboro.history.Proposal#getSols <em>Sols</em>}</li>
+ *   <li>{@link fr.inria.atlanmod.collaboro.history.Proposal#getSelected <em>Selected</em>}</li>
+ *   <li>{@link fr.inria.atlanmod.collaboro.history.Proposal#getVersion <em>Version</em>}</li>
+ *   <li>{@link fr.inria.atlanmod.collaboro.history.Proposal#isAccepted <em>Accepted</em>}</li>
+ *   <li>{@link fr.inria.atlanmod.collaboro.history.Proposal#getMeta <em>Meta</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see fr.inria.atlanmod.collaboro.history.HistoryPackage#getProposal()
+ * @model
+ * @generated
+ */
+public interface Proposal extends Collaboration {
+	/**
+	 * Returns the value of the '<em><b>Sols</b></em>' containment reference list.
+	 * The list contents are of type {@link fr.inria.atlanmod.collaboro.history.Solution}.
+	 * It is bidirectional and its opposite is '{@link fr.inria.atlanmod.collaboro.history.Solution#getProposal <em>Proposal</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Sols</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Sols</em>' containment reference list.
+	 * @see fr.inria.atlanmod.collaboro.history.HistoryPackage#getProposal_Sols()
+	 * @see fr.inria.atlanmod.collaboro.history.Solution#getProposal
+	 * @model opposite="proposal" containment="true"
+	 * @generated
+	 */
+	EList<Solution> getSols();
+
+	/**
+	 * Returns the value of the '<em><b>Selected</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Selected</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Selected</em>' reference.
+	 * @see #setSelected(Solution)
+	 * @see fr.inria.atlanmod.collaboro.history.HistoryPackage#getProposal_Selected()
+	 * @model
+	 * @generated
+	 */
+	Solution getSelected();
+
+	/**
+	 * Sets the value of the '{@link fr.inria.atlanmod.collaboro.history.Proposal#getSelected <em>Selected</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Selected</em>' reference.
+	 * @see #getSelected()
+	 * @generated
+	 */
+	void setSelected(Solution value);
+
+	/**
+	 * Returns the value of the '<em><b>Version</b></em>' container reference.
+	 * It is bidirectional and its opposite is '{@link fr.inria.atlanmod.collaboro.history.Version#getProposals <em>Proposals</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Version</em>' container reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Version</em>' container reference.
+	 * @see #setVersion(Version)
+	 * @see fr.inria.atlanmod.collaboro.history.HistoryPackage#getProposal_Version()
+	 * @see fr.inria.atlanmod.collaboro.history.Version#getProposals
+	 * @model opposite="proposals" transient="false"
+	 * @generated
+	 */
+	Version getVersion();
+
+	/**
+	 * Sets the value of the '{@link fr.inria.atlanmod.collaboro.history.Proposal#getVersion <em>Version</em>}' container reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Version</em>' container reference.
+	 * @see #getVersion()
+	 * @generated
+	 */
+	void setVersion(Version value);
+
+	/**
+	 * Returns the value of the '<em><b>Accepted</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Accepted</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Accepted</em>' attribute.
+	 * @see #setAccepted(boolean)
+	 * @see fr.inria.atlanmod.collaboro.history.HistoryPackage#getProposal_Accepted()
+	 * @model
+	 * @generated
+	 */
+	boolean isAccepted();
+
+	/**
+	 * Sets the value of the '{@link fr.inria.atlanmod.collaboro.history.Proposal#isAccepted <em>Accepted</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Accepted</em>' attribute.
+	 * @see #isAccepted()
+	 * @generated
+	 */
+	void setAccepted(boolean value);
+
+	/**
+	 * Returns the value of the '<em><b>Meta</b></em>' containment reference list.
+	 * The list contents are of type {@link fr.inria.atlanmod.collaboro.history.MetaInfo}.
+	 * It is bidirectional and its opposite is '{@link fr.inria.atlanmod.collaboro.history.MetaInfo#getProposal <em>Proposal</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Meta</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Meta</em>' containment reference list.
+	 * @see fr.inria.atlanmod.collaboro.history.HistoryPackage#getProposal_Meta()
+	 * @see fr.inria.atlanmod.collaboro.history.MetaInfo#getProposal
+	 * @model opposite="proposal" containment="true"
+	 * @generated
+	 */
+	EList<MetaInfo> getMeta();
+
+
+	String getStringState();
+	
+} // Proposal
---
+++ plugins/fr.inria.atlanmod.collaboro.history/src/fr/inria/atlanmod/collaboro/history/impl/HistoryImpl.java
@@ -0,0 +1,263 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package fr.inria.atlanmod.collaboro.history.impl;
+
+import fr.inria.atlanmod.collaboro.history.History;
+import fr.inria.atlanmod.collaboro.history.HistoryPackage;
+import fr.inria.atlanmod.collaboro.history.User;
+import fr.inria.atlanmod.collaboro.history.Version;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>History</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link fr.inria.atlanmod.collaboro.history.impl.HistoryImpl#getUsers <em>Users</em>}</li>
+ *   <li>{@link fr.inria.atlanmod.collaboro.history.impl.HistoryImpl#getVersions <em>Versions</em>}</li>
+ *   <li>{@link fr.inria.atlanmod.collaboro.history.impl.HistoryImpl#getLanguage <em>Language</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class HistoryImpl extends EObjectImpl implements History {
+	/**
+	 * The cached value of the '{@link #getUsers() <em>Users</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getUsers()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<User> users;
+
+	/**
+	 * The cached value of the '{@link #getVersions() <em>Versions</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getVersions()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Version> versions;
+
+	/**
+	 * The default value of the '{@link #getLanguage() <em>Language</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getLanguage()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String LANGUAGE_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getLanguage() <em>Language</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getLanguage()
+	 * @generated
+	 * @ordered
+	 */
+	protected String language = LANGUAGE_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected HistoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return HistoryPackage.Literals.HISTORY;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<User> getUsers() {
+		if (users == null) {
+			users = new EObjectContainmentEList<User>(User.class, this, HistoryPackage.HISTORY__USERS);
+		}
+		return users;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Version> getVersions() {
+		if (versions == null) {
+			versions = new EObjectContainmentEList<Version>(Version.class, this, HistoryPackage.HISTORY__VERSIONS);
+		}
+		return versions;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setLanguage(String newLanguage) {
+		String oldLanguage = language;
+		language = newLanguage;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, HistoryPackage.HISTORY__LANGUAGE, oldLanguage, language));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case HistoryPackage.HISTORY__USERS:
+				return ((InternalEList<?>)getUsers()).basicRemove(otherEnd, msgs);
+			case HistoryPackage.HISTORY__VERSIONS:
+				return ((InternalEList<?>)getVersions()).basicRemove(otherEnd, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case HistoryPackage.HISTORY__USERS:
+				return getUsers();
+			case HistoryPackage.HISTORY__VERSIONS:
+				return getVersions();
+			case HistoryPackage.HISTORY__LANGUAGE:
+				return getLanguage();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case HistoryPackage.HISTORY__USERS:
+				getUsers().clear();
+				getUsers().addAll((Collection<? extends User>)newValue);
+				return;
+			case HistoryPackage.HISTORY__VERSIONS:
+				getVersions().clear();
+				getVersions().addAll((Collection<? extends Version>)newValue);
+				return;
+			case HistoryPackage.HISTORY__LANGUAGE:
+				setLanguage((String)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case HistoryPackage.HISTORY__USERS:
+				getUsers().clear();
+				return;
+			case HistoryPackage.HISTORY__VERSIONS:
+				getVersions().clear();
+				return;
+			case HistoryPackage.HISTORY__LANGUAGE:
+				setLanguage(LANGUAGE_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case HistoryPackage.HISTORY__USERS:
+				return users != null && !users.isEmpty();
+			case HistoryPackage.HISTORY__VERSIONS:
+				return versions != null && !versions.isEmpty();
+			case HistoryPackage.HISTORY__LANGUAGE:
+				return LANGUAGE_EDEFAULT == null ? language != null : !LANGUAGE_EDEFAULT.equals(language);
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (language: ");
+		result.append(language);
+		result.append(')');
+		return result.toString();
+	}
+
+} //HistoryImpl
