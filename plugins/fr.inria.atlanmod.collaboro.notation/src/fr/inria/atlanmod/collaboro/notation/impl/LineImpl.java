1f9b092435a8fbb7de377f21f8a724e38da5406a
jlcanovas <me@jlcanovas.es>
2012-08-02 07:56:27 +0000

    preparing for tag 0.2
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/collaboro/trunk@118 ad8d3fff-70aa-a484-856a-f7f39f4f7e1d

---
+++ plugins/fr.inria.atlanmod.collaboro.notation/src/fr/inria/atlanmod/collaboro/notation/impl/LineImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package fr.inria.atlanmod.collaboro.notation.impl;
+
+import fr.inria.atlanmod.collaboro.notation.Line;
+import fr.inria.atlanmod.collaboro.notation.NotationPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Line</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class LineImpl extends GraphicalElementImpl implements Line {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected LineImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return NotationPackage.Literals.LINE;
+	}
+
+} //LineImpl
