f23ef2aaa7d3b0d88e0ca2accd1bd5afabf32e96
Juan David Villa Calle <juan-david.villa_calle@inria.fr>
2014-05-20 15:14:05 +0200

    Updated web backend to support enriched controller and local model manager.

--- plugins/fr.inria.atlanmod.collaboro.web.backend/src/fr/inria/atlanmod/collaboro/web/backend/Controller.java
+++ plugins/fr.inria.atlanmod.collaboro.web.backend/src/fr/inria/atlanmod/collaboro/web/backend/Controller.java
@@ -1,24 +1,289 @@
 package fr.inria.atlanmod.collaboro.web.backend;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EModelElement;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 
+import fr.inria.atlanmod.collaboro.history.AbstractSyntaxElement;
+import fr.inria.atlanmod.collaboro.history.Collaboration;
+import fr.inria.atlanmod.collaboro.history.Comment;
+import fr.inria.atlanmod.collaboro.history.ConcreteSyntaxElement;
+import fr.inria.atlanmod.collaboro.history.ExistingAbstractSyntaxElement;
 import fr.inria.atlanmod.collaboro.history.History;
-import fr.inria.atlanmod.collaboro.web.backend.LocalModelManager;
+import fr.inria.atlanmod.collaboro.history.HistoryFactory;
+import fr.inria.atlanmod.collaboro.history.ModelChange;
+import fr.inria.atlanmod.collaboro.history.NewAbstractSyntaxElement;
+import fr.inria.atlanmod.collaboro.history.Proposal;
+import fr.inria.atlanmod.collaboro.history.Solution;
+import fr.inria.atlanmod.collaboro.history.User;
+import fr.inria.atlanmod.collaboro.history.Version;
+import fr.inria.atlanmod.collaboro.history.VersionHistory;
+import fr.inria.atlanmod.collaboro.notation.Definition;
+import fr.inria.atlanmod.collaboro.notation.NotationElement;
+//import fr.inria.atlanmod.collaboro.ui.LocalModelManager;
+//import fr.inria.atlanmod.collaboro.ui.ModelManager;
+//import fr.inria.atlanmod.collaboro.ui.ModelManagerFactory;
 
+/**
+ * 
+ * @author Juan David Villa Calle
+ *
+ */
 public class Controller
 {
 
-	LocalModelManager modelManager;
+	// Variables to control the state of the plugin
+	private User loggedUser = null;
+	private int historyTracked = 0;
+	private int versionTracked = 0;
+	private int lastIndex = 0;
 	
-	public Controller(URI uriHistoryModel)
+	ModelManagerFactory modelManagerFactory = new ModelManagerFactory();
+	ModelManager modelManager = modelManagerFactory.createEmptyModelManager();
+
+	public Controller(URI modelBeingTracked)
 	{
-		modelManager = new LocalModelManager();
-		modelManager.initialize(uriHistoryModel);
+		if(modelManager instanceof LocalModelManager)
+		{
+			boolean initialized=((LocalModelManager) modelManager).initialize(new File(modelBeingTracked.toString()));
+			System.out.println("Inicializo al modelManager? "+initialized);
+		}
 	}
 	
-	public History getHistory()
+	/**
+	 * Testing method to reset the information
+	 */
+	public void reset() {
+		loggedUser = null;
+		lastIndex = 0;
+		historyTracked = 0;
+		versionTracked = 0;
+	}
+	
+	//public Controller(URI uriHistoryModel)
+	//{
+		//modelManager = new LocalModelManager();
+		//modelManager.initialize(uriHistoryModel);
+	//}
+	
+	public User getLoggedUser()
 	{
+			return loggedUser;
+	}	
+		
+	public History getHistory() {
 		return modelManager.getHistory();
 	}
+
+	public EPackage getEcoreModel() {
+		return modelManager.getEcoreModel();
+	}
+
+	public Definition getNotation() {
+		return modelManager.getNotation();
+	}
+	
+	public ModelManager getModelManager() 
+	{
+		return modelManager;
+	}
+	
+
+	
+	/**
+	 * Loads a new History model
+	 * 
+	 * @param resource to be tracked
+	 */
+	public void loadHistory(Object resource) { 
+		reset();
+		modelManager = modelManagerFactory.createModelManager(resource);
+		calculateLastIndexProposal();
+		if(getHistory() != null) 
+			versionTracked = getHistory().getHistories().get(historyTracked).getVersions().size() - 1;
+	}
+	
+	private void calculateLastIndexProposal() {
+		lastIndex = 0;
+
+		if(getHistory() != null) {
+			for(Proposal proposal : getHistory().getHistories().get(getHistoryTracked()).getVersions().get(getVersionTracked()).getProposals()) {
+				String idProposal = proposal.getId();
+				int valueProposal = Integer.valueOf(idProposal.substring(1, idProposal.length()));
+				if(valueProposal > lastIndex) 
+					lastIndex = valueProposal;
+				for(Comment comment : proposal.getComments()) { 
+					String idComment = comment.getId();
+					int valueComment = Integer.valueOf(idComment.substring(1, idComment.length()));
+					if(valueComment > lastIndex) 
+						lastIndex = valueComment;
+				}
+				for(Solution solution : proposal.getSols()) {
+					String idSolution = solution.getId();
+					int valueSolution = Integer.valueOf(idSolution.substring(1, idSolution.length()));
+					if(valueSolution > lastIndex) 
+						lastIndex = valueSolution;
+					for(Comment comment : solution.getComments()) {
+						String idCommentSol = comment.getId();
+						int valueCommentSol = Integer.valueOf(idCommentSol.substring(1, idCommentSol.length()));
+						if(valueCommentSol > lastIndex) 
+							lastIndex = valueCommentSol;
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Loads an ecore model. Normally the model conforms to the metamodel being tracked.
+	 * 
+	 * @param modelPath
+	 * @return
+	 */
+	public EObject loadModel(File modelPath) {
+		if(modelManager == null) return null;
+		return modelManager.loadModel(modelPath);
+	}
+	
+	/**
+	 * Sets the user logged in the application. 
+	 * 
+	 * @param userName
+	 * @return True if the user was found, false if not
+	 */
+	public boolean loginUser(String userName) {
+		boolean found = false;
+
+		if(getHistory() != null) {
+			for (User user : getHistory().getUsers()) {
+				if(user.getId().equals(userName)) {
+					loggedUser = user;
+					found = true;
+					break;
+				}
+			}
+		}
+
+		return found;
+	}
+	
+	/**
+	 * Indicates if the user is logged
+	 * @return
+	 */
+	public boolean isLogged() {
+		return !(loggedUser == null);
+	}
+	
+	public void createSolution(Proposal proposal) {
+		Solution newSolution = HistoryFactory.eINSTANCE.createSolution();
+		newSolution.setProposedBy(loggedUser);
+		newSolution.setId("n" + ++lastIndex);
+		proposal.getSols().add(newSolution);	
+		modelManager.saveHistory();
+		modelManager.saveNotation();
+		//refreshVersionView();
+	}
+
+
+	public void createProposal() {
+		Proposal newProposal = HistoryFactory.eINSTANCE.createProposal();
+		newProposal.setId("n" + ++lastIndex);
+		//newProposal.setProposedBy(loggedUser);
+		getProposals().add(newProposal);
+		Version version = getHistory().getHistories().get(getHistoryTracked()).getVersions().get(getVersionTracked());
+		version.getProposals().add(newProposal);
+		modelManager.saveHistory();
+		modelManager.saveNotation();
+		//refreshVersionView();
+	}
+
+	public void createComment(Collaboration collaboration) {
+		Comment newComment = HistoryFactory.eINSTANCE.createComment();
+		//newComment.setProposedBy(loggedUser);
+		newComment.setId("n" + ++lastIndex);
+		collaboration.getComments().add(newComment);
+		modelManager.saveHistory();
+		modelManager.saveNotation();
+		//refreshVersionView();
+	}
+	
+	
+	
+	public int getHistoryTracked() {
+		return historyTracked;
+	}
+	
+	public int getVersionTracked() {
+		return versionTracked;
+	}
+	
+	/**
+	 * Looks for a ConcreteSyntaxElement linked with the Eclassifier received
+	 * 
+	 * @param modelElement
+	 * @return
+	 */
+	public NotationElement getNotation(EClassifier modelElement) {
+		if(getHistory() == null) return null;
+
+		List<Proposal> proposals = getHistory().getHistories().get(getHistoryTracked()).getVersions().get(getVersionTracked()).getProposals();
+		for(Proposal proposal : proposals) {
+			List<Solution> solutions = proposal.getSols();
+			for(Solution solution : solutions) {
+				List<ModelChange> changes = solution.getChanges();
+				for(ModelChange change : changes) {
+					if(change.getReferredElement() instanceof AbstractSyntaxElement && change.getTarget() instanceof ConcreteSyntaxElement) {
+						EModelElement refModelElement = null;
+						if (change.getReferredElement() instanceof ExistingAbstractSyntaxElement) {
+							refModelElement = ((ExistingAbstractSyntaxElement) change.getReferredElement()).getElement();
+						}
+						if (change.getReferredElement() instanceof NewAbstractSyntaxElement) {
+							refModelElement = ((NewAbstractSyntaxElement) change.getReferredElement()).getElement();
+						}
+						if(refModelElement != null) {
+							if (refModelElement instanceof EClassifier) {
+								EClassifier classifier = (EClassifier) refModelElement;
+								if(classifier.getName().equals(modelElement.getName())) {
+									ConcreteSyntaxElement concreteElement = (ConcreteSyntaxElement) change.getTarget();
+									NotationElement result = concreteElement.getElement();
+									return result;
+								}
+							}
+						} 
+					} 
+				}
+			}
+		}
+
+		return null;
+	}
+	
+	public List<Proposal> getProposals() {
+		List<Proposal> result = new ArrayList<Proposal>();
+
+		if(getHistory() != null && getHistory().getHistories() != null) {
+			VersionHistory versionHistory = getHistory().getHistories().get(getHistoryTracked());
+			if(versionHistory != null && getHistory().getHistories().get(getHistoryTracked()).getVersions() != null) {
+				Version version = getHistory().getHistories().get(getHistoryTracked()).getVersions().get(getVersionTracked());
+				if(version != null) {
+					result = getHistory().getHistories().get(getHistoryTracked()).getVersions().get(getVersionTracked()).getProposals();
+				}
+			}
+		}
+		return result;
+	}
+	
+	public void saveHistory() {
+		modelManager.saveHistory();
+	}
+	
+	
 	
 }
--- plugins/fr.inria.atlanmod.collaboro.web.backend/src/fr/inria/atlanmod/collaboro/web/backend/LocalModelManager.java
+++ plugins/fr.inria.atlanmod.collaboro.web.backend/src/fr/inria/atlanmod/collaboro/web/backend/LocalModelManager.java
@@ -1,56 +1,120 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2012
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Javier Canovas (javier.canovas@inria.fr) 
+ *******************************************************************************/
+
 package fr.inria.atlanmod.collaboro.web.backend;
 
 import java.io.File;
 import java.io.IOException;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 
-
-
 import fr.inria.atlanmod.collaboro.history.History;
 import fr.inria.atlanmod.collaboro.history.HistoryPackage;
+import fr.inria.atlanmod.collaboro.notation.Definition;
+import fr.inria.atlanmod.collaboro.notation.NotationFactory;
+import fr.inria.atlanmod.collaboro.notation.NotationPackage;
 
-public class LocalModelManager {
-	
+public class LocalModelManager extends ModelManager {
 
-	//TODO Replace with url received from servlet
-	public static final String HISTORY_MODEL = "/model/ModiscoWorkflow.history";
-	
-	// Extensions for the models supported
-	public static final String HISTORY_EXTENSION = "history";
-	public static final String MODEL_EXTENSION = "xmi";
-	
 	// Track of history model
-	private URI historyModelUri;
+	private File historyModelFile;
 	private History history;
+	
+	// Track of abstract syntax
+	private File ecoreModelFile;
+	private EPackage ePackage;
+
+	// Track of concrete syntax
+	private File notationModelFile;
+	private Definition notationDefinition;
 	
 	// Resource unified
 	ResourceSet rset = null;
 	
-	public void initialize(URI historyModelUri)
-	{
+	public File getHistoryFile() {
+		return historyModelFile;
+	}
+	public History getHistory() {
+		return history;
+	}
+
+	public File getEcoreModelFile() {
+		return ecoreModelFile;
+	}
+	public EPackage getEcoreModel() {
+		return ePackage;
+	}
+
+	public File getNotationFile() {
+		return notationModelFile;
+	}
+	public Definition getNotation() {
+		return notationDefinition;
+	}
+	
+	public boolean initialize(File modelBeingTracked) {
 		
+		System.out.println("El archivo que esta llegando del modelo ecore: "+modelBeingTracked.getAbsolutePath());
+		if(!modelBeingTracked.getName().endsWith(".ecore")) 
+			return false;
+
+		if(ecoreModelFile != null && modelBeingTracked.getAbsoluteFile().equals(ecoreModelFile.getAbsoluteFile())) {
+			return false;
+		} 
+
+		// Inferring nameFiles
+		historyModelFile = inferHistoryModel(modelBeingTracked);		
+		if(! historyModelFile.exists()) {
+			System.out.println("No History found");
+			ecoreModelFile = null;
+			return false;
+		} else {
+			ecoreModelFile = modelBeingTracked;
+			notationModelFile = inferNotationModel(historyModelFile);
+
 			// Preparing the resource
 			rset = new ResourceSetImpl();
 			rset.getResourceFactoryRegistry().getExtensionToFactoryMap().put(HISTORY_EXTENSION, new EcoreResourceFactoryImpl());
-			rset.getResourceFactoryRegistry().getExtensionToFactoryMap().put(MODEL_EXTENSION, new EcoreResourceFactoryImpl());
 			rset.getPackageRegistry().put(HistoryPackage.eNS_URI, HistoryPackage.eINSTANCE);
-			
+			rset.getPackageRegistry().put(NotationPackage.eNS_URI, NotationPackage.eINSTANCE);
+
+			rset.getResourceFactoryRegistry().getExtensionToFactoryMap().put(MODEL_EXTENSION, new EcoreResourceFactoryImpl());
+			rset.getResourceFactoryRegistry().getExtensionToFactoryMap().put(NOTATION_EXTENSION, new EcoreResourceFactoryImpl());
+			rset.getResourceFactoryRegistry().getExtensionToFactoryMap().put(ECORE_EXTENSION, new EcoreResourceFactoryImpl());
+
 			// Loading the history model
-			this.historyModelUri=historyModelUri;
-			history = loadHistory();
+			loadHistory();
+			// Loading the ecore model (abstract syntax)
+			loadEcoreModel();
+			// Loading the notation model (concrete syntax)
+			loadNotation();
+			
+			return true;
+		}
 	}
 	
+	@Override
 	public History loadHistory() {
 		if(rset == null) return null;
 
 		History result = null;
 		
-		Resource res = rset.getResource(historyModelUri,true);
+		if(historyModelFile != null && historyModelFile.exists()) {
+			Resource res = rset.getResource(URI.createFileURI(historyModelFile.getAbsolutePath()), true);
 			try {
 				res.load(null);
 			} catch (IOException e) {
@@ -58,19 +122,157 @@ public class LocalModelManager {
 			}
 
 			result = (History) res.getContents().get(0);
-		
+		}
 		
 		history = result;
 		
 		return result;
 	}
-	
-	public History getHistory()
-	{
-		return history;
+
+	@Override
+	public Definition loadNotation() {
+		if(rset == null) return null;
+
+		Definition result = null;
 		
+		if(notationModelFile != null && notationModelFile.exists()) { 
+			Resource res3 = rset.getResource(URI.createFileURI(notationModelFile.getAbsolutePath()), true);
+			try {
+				res3.load(null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			result = (Definition) res3.getContents().get(0);
+		} else {
+			result = NotationFactory.eINSTANCE.createDefinition();
+		}
+		
+		notationDefinition = result;
+		
+		return result;
 	}
 	
+
+	public EPackage loadEcoreModel() {
+		if(rset == null) return null;
+		
+		EPackage result = null;
+		
+		if(ecoreModelFile != null && ecoreModelFile.exists()) {
+			Resource res2 = rset.getResource(URI.createFileURI(ecoreModelFile.getAbsolutePath()), true);
+			try {
+				res2.load(null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			result = (EPackage) res2.getContents().get(0);
+		}
+		
+		ePackage = result;
+		
+		return result;
+	}
 	
+
+	public EObject loadModel(File modelPath) {
+		if(rset != null && ePackage != null) {
+			rset.getPackageRegistry().put(ePackage.getNsURI(), ePackage);
+
+			rset.getResourceFactoryRegistry().getExtensionToFactoryMap().put(MODEL_EXTENSION, new EcoreResourceFactoryImpl());
+
+			Resource res = rset.getResource(URI.createFileURI(modelPath.getAbsolutePath()), true);
+			try {
+				res.load(null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			EObject eObject = res.getContents().get(0);
+			return eObject;
+		} 
+		return null;
+
+	}
+	
+
+	/**
+	 * Infers the path to the history model tracking the changes in modelBeingTracked
+	 * @param modelBeingTracked
+	 * @return
+	 */
+	private File inferHistoryModel(File modelBeingTracked) {
+		String parent =  modelBeingTracked.getParentFile().toString();
+		String name = modelBeingTracked.getName();
+		String historyName = name.substring(0, name.lastIndexOf(".")) + "." + HISTORY_EXTENSION;
+		return new File(parent + File.separator + historyName);
+	}
+
+	/**
+	 * Infers the path to the notatoin model
+	 * @param modelBeingTracked
+	 * @return
+	 */
+	private File inferNotationModel(File modelBeingTracked) {
+		String parent =  modelBeingTracked.getParentFile().toString();
+		String name = modelBeingTracked.getName();
+		String notationName = name.substring(0, name.lastIndexOf(".")) + "." + NOTATION_EXTENSION;
+		return new File(parent + File.separator + notationName);
+	}
+	
+	public void saveHistory() {
+		// Saving history model
+		if(historyModelFile != null && rset != null) {
+			Resource res = rset.getResource(URI.createFileURI(historyModelFile.getAbsolutePath()), true);
+
+			try {
+				res.getContents().clear();  // TODO fix this!
+				res.getContents().add(history);
+				res.save(null);
+			} catch (IOException e) {
+//				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void saveNotation() {
+		if(notationModelFile != null && rset != null) {
+			Resource res = null;
+
+			if(notationModelFile.exists()) 
+				res = rset.getResource(URI.createFileURI(notationModelFile.getAbsolutePath()), true);
+			else
+				res = rset.createResource(URI.createFileURI(notationModelFile.getAbsolutePath()));
+
+			try {
+				res.getContents().clear();  // TODO fix this!
+				res.getContents().add(notationDefinition);
+				res.save(null);
+			} catch (IOException e) {
+//				e.printStackTrace();
+			}
+		}
+	}
+	
+
+	public void saveEcoreModel() {
+		if(ecoreModelFile != null && rset != null) {
+			Resource res = rset.getResource(URI.createFileURI(ecoreModelFile.getAbsolutePath()), true);
+
+			try {
+				res.getContents().clear(); // TODO fix this!
+				res.getContents().add(ePackage);
+				res.save(null);
+			} catch (IOException e) {
+//				e.printStackTrace(); 
+			}
+		}
+	}
+	
+	@Override
+	public void closeConnection() {
+		this.saveEcoreModel();
+		this.saveHistory();
+		this.saveNotation();
+	}
 
 }
