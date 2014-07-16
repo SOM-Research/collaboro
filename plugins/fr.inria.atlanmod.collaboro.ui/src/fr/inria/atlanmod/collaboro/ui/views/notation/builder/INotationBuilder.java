6e1f74386139274cedf232b2e07f43039daf5bb2
jlcanovas <me@jlcanovas.es>
2014-07-16 16:40:02 +0200

    Fixing Notation view

--- plugins/fr.inria.atlanmod.collaboro.ui/src/fr/inria/atlanmod/collaboro/ui/views/notation/builder/INotationBuilder.java
+++ plugins/fr.inria.atlanmod.collaboro.ui/src/fr/inria/atlanmod/collaboro/ui/views/notation/builder/INotationBuilder.java
@@ -21,126 +21,36 @@ import fr.inria.atlanmod.collaboro.notation.Token;
 import fr.inria.atlanmod.collaboro.notation.Value;
 
 public interface INotationBuilder {
-
 	public String create(Keyword keyword); 
-	
-
-	
 	public String create(Token token); 
-	
-	
-
 	public String create(AttributeValue attributeValue); 
-	
-	
-
 	public String create(ReferenceValue referenceValue); 
-
-	
-	
 	public String create(Label label); 
-	
-	
-	
 	public String create(Line line);
-	
-	
-	
 	public String create(Rectangle rectangle);
-	
-	
-	
 	public String create(Composite composite);
-	
-	
-	
 	public String create(SyntaxOf syntaxOf);
-	
-	
-	
 	public String create(NotationElement notationElement);
-	
-	
-	
 	public String create(TextualElement textualElement);
-	
-	
-	
 	public String create(Value value);
-	
-	
-	
 	public String create(GraphicalElement graphicalElement);
-	
-	
-	
 	public String create(Figure figure);
-	
-	
-	
 	public String create(Image image);
-	
-	
-	
+
 	//with the associated EObjects
-	
-	
 	public String create(EObject eObject, Keyword keyword, Map<EObject, EObject> historyLinks); 
-	
-
-	
 	public String create(EObject eObject, Token token, Map<EObject, EObject> historyLinks); 
-	
-	
-
 	public String create(EObject eObject, AttributeValue attributeValue, Map<EObject, EObject> historyLinks); 
-	
-	
-
 	public String create(EObject eObject, ReferenceValue referenceValue, Map<EObject, EObject> historyLinks); 
-
-	
-	
 	public String create(EObject eObject, Label label, Map<EObject, EObject> historyLinks); 
-	
-	
-	
 	public String create(EObject eObject, Line line, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, Rectangle rectangle, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, Composite composite, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, SyntaxOf syntaxOf, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, NotationElement notationElement, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, TextualElement textualElement, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, Value value, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, GraphicalElement graphicalElement, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, Image image, Map<EObject, EObject> historyLinks);
-	
-	
-	
 	public String create(EObject eObject, Figure figure, Map<EObject, EObject> historyLinks);
-	
 }
