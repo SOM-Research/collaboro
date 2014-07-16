e4228844504f95c0a21450466d58a9ce49f2d6f8
Juan David Villa Calle <juan-david.villa_calle@inria.fr>
2014-07-16 09:24:44 +0200

    Cats removed

--- web-client/app/controllers/ImagesCtrl.js
+++ web-client/app/controllers/ImagesCtrl.js
@@ -1,9 +1,10 @@
 angular.module('collaboroControllers').controller('imagesCtrl', ['$scope',function($scope){
-  $scope.myInterval = -1;
-  var slides= $scope.slides  =[];
-  //var slides = $scope.slides = [{image:'http://www.eclipse.org/atl/usecases/MMIndApproachtoDiffRep/img/extendedUmlMetamodel.png', text:'Notation example 0'}];
+  $scope.myInterval = 5000;
+  //var slides= $scope.slides  =[];
+  var slides = $scope.slides = [{image:'http://www.eclipse.org/atl/usecases/MMIndApproachtoDiffRep/img/extendedUmlMetamodel.png', text:'Notation example 0'}];
   $scope.addSlide = function() {
-   var newWidth = 600 + slides.length;
+   //var newWidth = 600 + slides.length;
+   var newWidth = 600;
    slides.push({
     image: 'http://placekitten.com/' + newWidth + '/300',
     text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
@@ -12,6 +13,10 @@ angular.module('collaboroControllers').controller('imagesCtrl', ['$scope',functi
  };
  for (var i=0; i<4; i++)
  {
-  $scope.addSlide();
-}
+  //$scope.addSlide();
+ }
+
+ //$scope.sportImages = ['http://placekitten.com/600/300','http://placekitten.com/601/300','http://placekitten.com/602/300'];
+ //$scope.sportImages = ['http://placekitten.com/604/300','http://placekitten.com/601/300','http://placekitten.com/602/300'];
+
 }]);
